b6102c68e6d8efd84c79dd19ea49bae70a3c5242
JunSP13 <57160583+JunSP13@users.noreply.github.com>
2023-03-03 02:16:46 -0600

    Update EventListener.php

--- src/PerWorldChat/EventListener.php
+++ src/PerWorldChat/EventListener.php
@@ -34,7 +34,7 @@ class EventListener implements Listener {
 		    if($this->plugin->cfg["log-chat-disabled"]){
 		        $player->sendMessage(TextFormat::colorize(PerWorldChat::PREFIX . "&cChat is disabled on this world"));
 		    }
-		    $event->isCancelled()return true;
+		    $event->isCancelled = true;
 		}
 		$recipients = $event->getRecipients();
 		foreach($recipients as $key => $recipient){
